1b326d793904c6545308a29af02690c9417bd13b
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2017-10-08 18:56:43 -0400

    flesh out UI test

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,21 +1,63 @@
 import org.scalatestplus.play._
+import play.api.mvc.Result
 import play.api.test._
 import play.api.test.Helpers._
+
+import scala.collection.immutable
+import scala.concurrent.Future
+import scala.xml.{Elem, Node}
 
 
 class ApplicationSpec extends PlaySpec with OneAppPerTest {
 
   "TieTacToe" should {
 
-    "render the index page" in {
-      val home = route(app, FakeRequest(GET, "/")).get
+    "render a board for a new game" in {
+      val newGame = route(app, FakeRequest(GET, "/")).get
 
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      // TODO: legit UI testing
-      //contentAsString(home) must include ("Your new application is ready.")
+      status(newGame) mustBe OK
+      contentType(newGame) mustBe Some("text/html")
+      val page = pageAsXml(newGame)
+      pageHeading(page) mustBe("Unbeatable Tic Tac Toe")
+
+    }
+
+    "render a board with the computer going first for a new game" in {
+      val newGame = route(app, FakeRequest(GET, "/")).get
+
+      val page = pageAsXml(newGame)
+      board(page).matches("X")
+    }
+
+    "X blocks the win for a game in progress" in {
+      val newGame = route(app, FakeRequest(GET, "/AAOXOXAAA")).get
+
+      implicit val page = pageAsXml(newGame)
+      board mustBe("..OXOXX..")
     }
 
   }
 
+  private def pageHeading(implicit page: Elem) = {
+    (page \\ "h1" text)
+  }
+
+  private def board(implicit page: Elem) = {
+
+    def classNodeToCellState(classNode: Node) = {
+       val stateClass = classNode.text.split(" ")(1)
+       if (stateClass == "Clear") "." else stateClass
+    }
+
+    (page \\ "table" \ "tr" \ "td")
+      .map(_.attribute("class")).flatten.flatten.map(classNodeToCellState).mkString
+
+  }
+
+  private def pageAsXml(eventualResult: Future[Result]) = {
+    scala.xml.XML.loadString(contentAsString(eventualResult).replaceFirst("<!DOCTYPE html>", ""))
+  }
 }
+
+
+
